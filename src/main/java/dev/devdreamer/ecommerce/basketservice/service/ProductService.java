9e48e1bc9baae88f8e8065b18bbbd442c9850dbb
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-04 14:34:24 -0300

    feat: add ProductService

--- src/main/java/dev/devdreamer/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/service/ProductService.java
@@ -1,4 +1,25 @@
 package dev.devdreamer.ecommerce.basketservice.service;
 
+import dev.devdreamer.ecommerce.basketservice.client.PlatzStoreClient;
+import dev.devdreamer.ecommerce.basketservice.client.response.PlatziProductResponse;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
 public class ProductService {
+    private final PlatzStoreClient platzStoreClient;
+
+    public ProductService(PlatzStoreClient platzStoreClient) {
+        this.platzStoreClient = platzStoreClient;
+    }
+
+    public List<PlatziProductResponse> getAllProducts (){
+        return platzStoreClient.getAllProducts();
+    }
+
+    public PlatziProductResponse getProductById (Long id){
+        return  platzStoreClient.getProductById(id);
+    }
+
 }
