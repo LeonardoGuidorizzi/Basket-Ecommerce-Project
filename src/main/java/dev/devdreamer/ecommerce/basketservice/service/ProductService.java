4ed456fec75b837109197eb825c9947be90c4443
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-10 00:17:26 -0300

    feat: add log to see if the caching is working

--- src/main/java/dev/devdreamer/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/service/ProductService.java
@@ -2,11 +2,12 @@ package dev.devdreamer.ecommerce.basketservice.service;
 
 import dev.devdreamer.ecommerce.basketservice.client.PlatzStoreClient;
 import dev.devdreamer.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-
+@Slf4j
 @Service
 public class ProductService {
     private final PlatzStoreClient platzStoreClient;
@@ -16,10 +17,12 @@ public class ProductService {
     }
     @Cacheable(value = "products")
     public List<PlatziProductResponse> getAllProducts (){
+        log.info("getting all products");
         return platzStoreClient.getAllProducts();
     }
     @Cacheable(value = "product", key = "#id")
     public PlatziProductResponse getProductById (Long id){
+        log.info("getting products by id:{}", id);
         return  platzStoreClient.getProductById(id);
     }
 
