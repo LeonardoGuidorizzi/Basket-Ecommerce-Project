e374a0aa2b067ad0852540bae6b9c50b7b78a4b9
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-15 16:22:16 -0300

    feat: add exception part like resourceNotFound, BusinessException and Handler... also add Response error, add Mappers to basket and order and dtos

---
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/dto/order/OrderResponseDTO.java
@@ -0,0 +1,4 @@
+package dev.devdreamer.ecommerce.basketservice.dto.order;
+
+public class OrderResponseDTO {
+}
---
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package dev.devdreamer.ecommerce.basketservice.exception.handler;
+
+public class GlobalExceptionHandler {
+}
