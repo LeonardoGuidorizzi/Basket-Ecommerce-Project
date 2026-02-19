3dba94b5a0ff6730ab2ff478ab08fe2bda9a804e
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-19 04:00:28 -0300

    feat: turn the product from api to domain product

--- src/main/java/dev/devdreamer/ecommerce/basketservice/mapper/ProductMapper.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/mapper/ProductMapper.java
@@ -2,23 +2,24 @@ package dev.devdreamer.ecommerce.basketservice.mapper;
 
 import dev.devdreamer.ecommerce.basketservice.client.response.PlatziProductResponse;
 import dev.devdreamer.ecommerce.basketservice.domain.product.Product;
-import dev.devdreamer.ecommerce.basketservice.exception.custom.BusinessException;
 
 public class ProductMapper {
     public static Product toDomain(PlatziProductResponse response) {
-
-        if (response == null) {
-            throw new IllegalArgumentException("Product response cannot be null");
-        }
-
-        if (response.price() == null) {
-            throw new BusinessException("Product price cannot be null");
-        }
-
         return Product.builder()
-                .id(response.getId())
-                .title(response.getTitle())
-                .price(response.getPrice())
+                .id(response.id())
+                .name(response.title())
+                .slug(response.slug())
+                .price(response.price())
+                .description(response.description())
                 .build();
+
+//        if (response == null) {
+//            throw new IllegalArgumentException("Product response cannot be null");
+//        }
+//
+//        if (response.price() == null) {
+//            throw new IllegalArgumentException("Product price cannot be null");
+//        }
+
     }
 }
