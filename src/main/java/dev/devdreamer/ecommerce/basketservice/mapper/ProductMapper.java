4a6a6b60bd9128d962089ab690dbee0414e8b022
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-18 21:17:38 -0300

    feat: take the atributes off the api an place in my product domain

--- src/main/java/dev/devdreamer/ecommerce/basketservice/mapper/ProductMapper.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/mapper/ProductMapper.java
@@ -1,4 +1,24 @@
 package dev.devdreamer.ecommerce.basketservice.mapper;
 
+import dev.devdreamer.ecommerce.basketservice.client.response.PlatziProductResponse;
+import dev.devdreamer.ecommerce.basketservice.domain.product.Product;
+import dev.devdreamer.ecommerce.basketservice.exception.custom.BusinessException;
+
 public class ProductMapper {
+    public static Product toDomain(PlatziProductResponse response) {
+
+        if (response == null) {
+            throw new IllegalArgumentException("Product response cannot be null");
+        }
+
+        if (response.price() == null) {
+            throw new BusinessException("Product price cannot be null");
+        }
+
+        return Product.builder()
+                .id(response.getId())
+                .title(response.getTitle())
+                .price(response.getPrice())
+                .build();
+    }
 }
