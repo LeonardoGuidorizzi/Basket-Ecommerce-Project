bdc14563aeed118ac91b8670adcd4b33246ef488
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-19 14:35:01 -0300

    feat: change where was the auth, i'm placing the authService with User detail service and and creating the UserService with login and register method

--- src/main/java/dev/devdreamer/ecommerce/basketservice/security/auth/AuthService.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/security/auth/AuthService.java
@@ -1,47 +1,19 @@
 package dev.devdreamer.ecommerce.basketservice.security.auth;
-
-import dev.devdreamer.ecommerce.basketservice.domain.user.User;
-import dev.devdreamer.ecommerce.basketservice.dto.auth.LoginRequestDTO;
-import dev.devdreamer.ecommerce.basketservice.dto.auth.LoginResponseDTO;
-import dev.devdreamer.ecommerce.basketservice.dto.auth.RegisterRequestDTO;
 import dev.devdreamer.ecommerce.basketservice.repository.UserRepository;
-import dev.devdreamer.ecommerce.basketservice.security.jwt.TokenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
-
+public class AuthService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final TokenService tokenService;
 
-    public String register(RegisterRequestDTO request) {
-
-        if (userRepository.findByEmail(request.email()).isPresent()) {
-            throw new RuntimeException("Email already in use");
-        }
-
-        String encodedPassword = passwordEncoder.encode(request.password());
-
-        User user = User.create( request.email(), encodedPassword);
-
-        userRepository.save(user);
-
-        return tokenService.generateToken(user);
-    }
-
-    public LoginResponseDTO login(LoginRequestDTO request) {
-
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                request.email(),
-                request.password()
-        );
-        User user = (User) authenticationToken.getPrincipal();
-        String token = tokenService.generateToken(user);
-        return new LoginResponseDTO(token);
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
