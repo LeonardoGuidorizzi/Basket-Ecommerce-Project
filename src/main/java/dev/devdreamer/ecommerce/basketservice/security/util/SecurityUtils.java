d32ae00436dde8f559569529843d0b15365bcd67
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-18 21:33:43 -0300

    feat: add utility method to inject the user context in the requests

--- src/main/java/dev/devdreamer/ecommerce/basketservice/security/util/SecurityUtils.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/security/util/SecurityUtils.java
@@ -1,4 +1,13 @@
 package dev.devdreamer.ecommerce.basketservice.security.util;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class SecurityUtils {
+
+    public static String getAuthenticatedUserId() {
+        return (String) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+    }
 }
