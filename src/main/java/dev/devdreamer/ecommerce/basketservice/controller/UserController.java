fb7c160d75df19b95bfccd27c45dfef8d9fc29fc
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-19 04:06:32 -0300

    feat: add user Controller

--- src/main/java/dev/devdreamer/ecommerce/basketservice/controller/UserController.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/controller/UserController.java
@@ -1,4 +1,30 @@
 package dev.devdreamer.ecommerce.basketservice.controller;
 
+import dev.devdreamer.ecommerce.basketservice.dto.auth.LoginRequestDTO;
+import dev.devdreamer.ecommerce.basketservice.dto.auth.LoginResponseDTO;
+import dev.devdreamer.ecommerce.basketservice.dto.auth.RegisterRequestDTO;
+import dev.devdreamer.ecommerce.basketservice.security.auth.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/basketservice/auth")
+@RequiredArgsConstructor
 public class UserController {
+    private final AuthService authService;
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@Valid @RequestBody RegisterRequestDTO request){
+        authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request){
+        return ResponseEntity.status(HttpStatus.OK).body(authService.login(request));
+    }
 }
