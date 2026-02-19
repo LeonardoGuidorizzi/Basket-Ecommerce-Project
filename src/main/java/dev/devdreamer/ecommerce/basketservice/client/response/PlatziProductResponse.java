b7b895a2d98426f7e9b33f67086f04c9628cacd7
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-19 04:09:54 -0300

    feat: add builder on PlatziProductResponse to use on Product convertion

--- src/main/java/dev/devdreamer/ecommerce/basketservice/client/response/PlatziProductResponse.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/client/response/PlatziProductResponse.java
@@ -1,8 +1,11 @@
 package dev.devdreamer.ecommerce.basketservice.client.response;
 
+import lombok.Builder;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Builder
 public record PlatziProductResponse(
         Long id,
         String title,
