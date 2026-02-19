0b1a1b873cf3fb0a1560a0c354f94c95744c684c
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-19 14:34:31 -0300

    feat: change where was the auth, i'm placing the authService with User detail service and and creating the UserService with login and register method

--- src/main/java/dev/devdreamer/ecommerce/basketservice/service/UserService.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/service/UserService.java
@@ -1,4 +1,51 @@
 package dev.devdreamer.ecommerce.basketservice.service;
 
-public class UserServicec {
+import dev.devdreamer.ecommerce.basketservice.domain.user.User;
+import dev.devdreamer.ecommerce.basketservice.dto.auth.LoginRequestDTO;
+import dev.devdreamer.ecommerce.basketservice.dto.auth.LoginResponseDTO;
+import dev.devdreamer.ecommerce.basketservice.dto.auth.RegisterRequestDTO;
+import dev.devdreamer.ecommerce.basketservice.repository.UserRepository;
+import dev.devdreamer.ecommerce.basketservice.security.jwt.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    public String register(RegisterRequestDTO request) {
+
+        if (userRepository.findByEmail(request.email()).isPresent()) {
+            throw new RuntimeException("Email already in use");
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        User user = User.create( request.email(), encodedPassword);
+
+        userRepository.save(user);
+
+        return tokenService.generateToken(user);
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO request) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                request.email(),
+                request.password()
+        );
+        Authentication authentication =
+                authenticationManager.authenticate(authenticationToken);
+        User user = (User) authentication.getPrincipal();
+        String token = tokenService.generateToken(user);
+        return new LoginResponseDTO(token);
+    }
 }
