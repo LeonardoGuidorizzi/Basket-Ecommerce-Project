2bcb4b78ed6414e69c4325b87f2af1775cbf9a38
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-19 04:05:27 -0300

    feat: add the userDetail on userDomain

--- src/main/java/dev/devdreamer/ecommerce/basketservice/domain/user/User.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/domain/user/User.java
@@ -6,20 +6,26 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Document(collection = "user")
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     private String id;
-
+    @Indexed(unique = true)
     private String email;
 
     private String password;
@@ -35,5 +41,35 @@ public class User {
                 .role(UserRole.USER)
                 .createdAt(LocalDateTime.now())
                 .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
