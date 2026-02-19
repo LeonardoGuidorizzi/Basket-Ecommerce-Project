7e8cb48a46b9a4767022e641d20607318fdd52ac
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-18 22:08:14 -0300

    feat: add atributes on Onder item

--- src/main/java/dev/devdreamer/ecommerce/basketservice/domain/order/OrderItem.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/domain/order/OrderItem.java
@@ -1,4 +1,11 @@
-package dev.devdreamer.ecommerce.basketservice.domain;
+package dev.devdreamer.ecommerce.basketservice.domain.order;
+
+import java.math.BigDecimal;
 
 public class OrderItem {
-}
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal unitPriceAtOrder;// Preço do produto no momento do pedido
+    private String productNameAtOrder; // Nome do produto no momento do pedido
+    private BigDecimal subtotalAtOrder; // Subtotal do item no momento do pedido
+ }
