7f2abe1bf459decfaf3f8b84a9db909798d2609d
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-18 21:45:35 -0300

    feat: add userId and totalAmount with bigDecimal

--- src/main/java/dev/devdreamer/ecommerce/basketservice/domain/basket/Basket.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/domain/basket/Basket.java
@@ -1,19 +1,28 @@
-package dev.devdreamer.ecommerce.basketservice.domain;
+package dev.devdreamer.ecommerce.basketservice.domain.basket;
 
 
-import dev.devdreamer.ecommerce.basketservice.domain.Enum.BasketStatus;
-import dev.devdreamer.ecommerce.basketservice.domain.basket.BasketItem;
+import dev.devdreamer.ecommerce.basketservice.Enum.BasketStatus;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "basket")
 public class Basket {
     @Id
-    private Long id;
-    private List<BasketItem> itens;
-    private BasketStatus status = BasketStatus.OPEN;
+    private String id;
+    @Indexed
+    private String userId;
+
+    private List<BasketItem> items = new ArrayList<>();
+
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    private BasketStatus status;
     private LocalDateTime createdAt;
     private LocalDateTime updateAt;
 
