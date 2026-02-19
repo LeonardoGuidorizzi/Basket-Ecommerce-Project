48d3aabfcd81e9597160eff045a6f420600b522b
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-18 21:16:29 -0300

    feat: turn product domain in utility class to be useful to take some atributes from api

--- src/main/java/dev/devdreamer/ecommerce/basketservice/domain/product/Product.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/domain/product/Product.java
@@ -1,9 +1,14 @@
 package dev.devdreamer.ecommerce.basketservice.domain.product;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
 import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
-
+@UtilityClass
+@Builder
+@Getter
 public class Product {
     @Id
     private Long id;
