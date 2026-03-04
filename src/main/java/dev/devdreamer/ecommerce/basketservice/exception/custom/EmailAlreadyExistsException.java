2e56b509ac3d914d09bb5eb295875ea605890ab0
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-03-04 14:18:55 -0300

    feat: add EmailAlreadyExistsException class

--- src/main/java/dev/devdreamer/ecommerce/basketservice/exception/custom/EmailAlreadyExistsException.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/exception/custom/EmailAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package dev.devdreamer.ecommerce.basketservice.exception.custom;
 
-public class EmailAlreadyExistsException {
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message){
+        super(message);
+    }
 }
