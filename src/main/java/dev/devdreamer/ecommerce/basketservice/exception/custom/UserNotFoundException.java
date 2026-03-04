830d4cfa26d449642bfc08fc559e1eaaf57fe935
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-03-04 14:14:11 -0300

    feat: delete UserNotFoundException, add tests, and add exceptions files

--- src/main/java/dev/devdreamer/ecommerce/basketservice/exception/custom/UserNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package dev.devdreamer.ecommerce.basketservice.exception.custom;
-
-public class UserNotFoundException {
-}
---
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/exception/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,4 @@
+package dev.devdreamer.ecommerce.basketservice.exception.security;
+
+public class CustomAccessDenied {
+}
