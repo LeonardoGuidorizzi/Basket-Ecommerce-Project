e603cd1a6596caff4906512b59685e3616e929a4
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-03-09 16:03:07 -0300

    feat: add orderbyuserId to use the user context

--- src/main/java/dev/devdreamer/ecommerce/basketservice/repository/OrderRepository.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package dev.devdreamer.ecommerce.basketservice.repository;
 
+import dev.devdreamer.ecommerce.basketservice.domain.basket.Basket;
 import dev.devdreamer.ecommerce.basketservice.domain.order.Order;
 import dev.devdreamer.ecommerce.basketservice.domain.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends MongoRepository<Order, String> {
+    List<Order> findByUserId(String userId);
     Optional<Order> findByIdAndUserId(String id, String userId);
 }
