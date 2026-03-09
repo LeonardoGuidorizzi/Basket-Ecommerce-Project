c22e813e0ea9036c15986b353040da78c4a9542e
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-03-09 15:59:50 -0300

    converse the the basket order to order

--- src/main/java/dev/devdreamer/ecommerce/basketservice/domain/order/OrderItem.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/domain/order/OrderItem.java
@@ -1,5 +1,6 @@
 package dev.devdreamer.ecommerce.basketservice.domain.order;
 
+import dev.devdreamer.ecommerce.basketservice.domain.basket.BasketItem;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,4 +13,14 @@ public class OrderItem {
     private BigDecimal unitPriceAtOrder;// Preço do produto no momento do pedido
     private Integer quantity;
     private BigDecimal subtotalAtOrder; // Subtotal do item no momento do pedido
+
+    public static OrderItem fromBasketItem(BasketItem item) {
+        return OrderItem.builder()
+                .productId(item.getProductId())
+                .productNameAtOrder(item.getProductName())
+                .quantity(item.getQuantity())
+                .subtotalAtOrder(item.getSubtotal())
+                .unitPriceAtOrder(item.getUnitPrice())
+                .build();
+    }
  }
