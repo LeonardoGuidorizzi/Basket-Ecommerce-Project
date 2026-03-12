8f81299f3571208d6060f21222029e65c6e7eaa3
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-03-11 21:28:06 -0300

    refactor: Exception message

--- src/main/java/dev/devdreamer/ecommerce/basketservice/exception/custom/ResourceNotFoundException.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/exception/custom/ResourceNotFoundException.java
@@ -4,4 +4,7 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message){
         super(message);
     }
+    public ResourceNotFoundException(String resourceName, Long id) {
+        super(String.format("%s with ID %d not found", resourceName, id));
+    }
 }
