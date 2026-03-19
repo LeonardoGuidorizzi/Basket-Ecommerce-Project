951b2d775ebf073033d98c581470875df39c1482
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-03-19 13:27:46 -0300

    doc: explain how the redis configuration works

--- src/main/java/dev/devdreamer/ecommerce/basketservice/configuration/RedisConfiguration.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/configuration/RedisConfiguration.java
@@ -19,6 +19,13 @@ public class RedisConfiguration {
         return new RedisProperties();
     }
 
+    /*
+    RedisStandaloneConfiguration (O quê):
+    Propósito: Armazenar detalhes de conexão de um único servidor Redis (Host, Porta, Password, Database).
+    Tipo: É um objeto de configuração simples.
+    Uso: Passado como argumento para criar o ConnectionFactory.
+     */
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
@@ -27,6 +34,12 @@ public class RedisConfiguration {
         config.setPassword(redisProperties.getPassword());
         return new LettuceConnectionFactory(config);
     }
+    /*
+    RedisConnectionFactory (Como):
+    Propósito: Criar conexões (RedisConnection) ativas com o servidor configurado.
+    Tipo: Interface (implementada por JedisConnectionFactory ou LettuceConnectionFactory).
+    Uso: Utilizado pelo RedisTemplate para realizar operações no Redis.
+    */
 
 
     @Data
@@ -36,3 +49,9 @@ public class RedisConfiguration {
         private String password;
     }
 }
+
+    /* A principal diferença entre Lettuce e Jedis é a arquitetura de conexão: Lettuce é assíncrono,
+    * thread-safe (compartilhável entre threads)
+    * e ideal para alto desempenho,enquanto Jedis é síncrono e requer pool de conexões,
+    * sendo mais simples, porém menos escalável. Lettuce usa Netty e é padrão no Spring Boot.
+    */
