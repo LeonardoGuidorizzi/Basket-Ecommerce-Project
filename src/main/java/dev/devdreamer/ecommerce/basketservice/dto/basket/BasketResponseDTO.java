2a933b458187080b77e63c43c41e2581e63bd351
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-08 21:43:25 -0300

    feat: turning Basket reponse serializable

--- src/main/java/dev/devdreamer/ecommerce/basketservice/dto/basket/BasketResponseDTO.java
+++ src/main/java/dev/devdreamer/ecommerce/basketservice/dto/basket/BasketResponseDTO.java
@@ -3,16 +3,18 @@ package dev.devdreamer.ecommerce.basketservice.dto.basket;
 import dev.devdreamer.ecommerce.basketservice.Enum.BasketStatus;
 import dev.devdreamer.ecommerce.basketservice.domain.basket.BasketItem;
 import lombok.Builder;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
-public record BasketResponseDTO (
+public record BasketResponseDTO  (
                 List<BasketItem> items,
                 BigDecimal totalAmount,
                 BasketStatus status,
                 LocalDateTime createdAt,
                 LocalDateTime updateAt
-) {
+)implements Serializable {
 }
